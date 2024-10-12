ebed8454c8a7724869806024b0a2a67fc3d2e3fa
BrayanRM23 <brayan.rodriguez04@unicatolica.edu.co>
2024-10-12 16:41:02 -0500

    leer7

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -9,9 +9,9 @@ function UserHome({ user }) {
     }
 
     const home = useNavigate();
+    const [textoSigno, setTextoSigno] = useState('');
     const [signo, setSigno] = useState('');
     const [genero, setGenero] = useState('');
-    const [textoSigno, setTextoSigno] = useState('');
 
     function goHome() {
         home("/");
@@ -34,21 +34,29 @@ function UserHome({ user }) {
                 console.error("Error al obtener el signo:", error);
                 setTextoSigno("Error al conectar con el servidor.");
             }
-        } else {
-            setTextoSigno("Seleccione un signo y un género.");
         }
+    }
+
+    function handleSignoChange(event) {
+        setSigno(event.target.value);
+    }
+
+    function handleGeneroChange(event) {
+        setGenero(event.target.value);
     }
 
     return (
         <div className="container">
-            <div id="txtSeleccionPage"><h3>Selecciona tu signo zodiacal</h3></div>
+            <div id="txtSeleccionPage">
+                <h3>Selecciona tu signo zodiacal</h3>
+            </div>
 
-            <div>
-                <select id="selectSigno" onChange={(e) => setSigno(e.target.value)}>
+            <div className="selectores">
+                <select id="selectSignos" onChange={handleSignoChange}>
                     <option value="">Seleccione un signo</option>
                     <option value="Aries">Aries</option>
-                    <option value="Geminis">Géminis</option>
-                    <option value="Cancer">Cáncer</option>
+                    <option value="Géminis">Géminis</option>
+                    <option value="Cáncer">Cáncer</option>
                     <option value="Leo">Leo</option>
                     <option value="Virgo">Virgo</option>
                     <option value="Libra">Libra</option>
@@ -59,7 +67,7 @@ function UserHome({ user }) {
                     <option value="Piscis">Piscis</option>
                 </select>
 
-                <select id="selectSigno" onChange={(e) => setGenero(e.target.value)}>
+                <select id="selectSignos" onChange={handleGeneroChange}>
                     <option value="">Seleccione un género</option>
                     <option value="hombre">Hombre</option>
                     <option value="mujer">Mujer</option>
@@ -67,17 +75,16 @@ function UserHome({ user }) {
                 </select>
             </div>
 
-            <button onClick={fetchSigno}>Ver Signo</button>
+            <button id="btnBuscar" onClick={fetchSigno}>Buscar</button>
 
             <TextSigno texto={textoSigno} />
 
-            <button id="btnHome" onClick={fetchSigno}>Ver Signo</button>
+            <button id="btnHome" onClick={fetchSigno}>Buscar</button>
         </div>
     );
 }
 
 export default UserHome;
-
 
 
 
